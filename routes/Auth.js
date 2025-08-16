f7bc5d2e9ab808532017f33b225d3f12fc18a81b
Ankit Prajapat <141832510+ankit-prajapati-24@users.noreply.github.com>
2025-08-16 12:05:21 +0530

    Update Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,7 +4,7 @@ const route = express.Router();
 const { Signup,Login} = require("../controllers/AuthController");
 
 route.post("/Signup", Signup);
-route.get("/Login",Login);
+route.post("/Login",Login);
 
 
 
